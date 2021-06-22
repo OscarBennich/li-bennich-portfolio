82e6ade1f20872786a775b6e8f0e4a7f97daac08
Oscar Bennich-Björkman <oscar.bennich@gmail.com>
2021-06-22 23:50:43 +0200

    Fix alignment in footer

--- src/components/WebsiteFooter.tsx
+++ src/components/WebsiteFooter.tsx
@@ -5,28 +5,33 @@ import { Segment, Container, Grid, Icon, Label } from "semantic-ui-react";
 const WebsiteFooter = () => {
   return (
     <Segment
-      inverted
       vertical
-      style={{ padding: "1.5em 0em 0em 0em", width: "100%" }}
+      color="black"
+      style={{
+        padding: "1.5em 0em 0em 0em",
+        width: "100%",
+      }}
     >
       <Container>
-        <Grid centered inverted stackable>
+        <Grid centered stackable>
           <Grid.Row>
-            <Grid.Column width={3}>
+            <Grid.Column width={3} verticalAlign="bottom">
+              <Label pointing="right">Made by</Label>
               <a
                 href="https://www.linkedin.com/in/oscar-bennich-bjorkman/"
                 rel="noreferrer"
                 target="_blank"
+                className="primaryColor"
               >
                 <Icon name="linkedin" size="big" />
               </a>
-              <Label pointing="left">Made by</Label>
             </Grid.Column>
-            <Grid.Column width={3}>
+            <Grid.Column width={3} verticalAlign="top">
               <a
                 href="https://github.com/OscarBennich/li-bennich-portfolio"
                 rel="noreferrer"
                 target="_blank"
+                className="primaryColor"
               >
                 <Icon name="github" size="big" />
               </a>
